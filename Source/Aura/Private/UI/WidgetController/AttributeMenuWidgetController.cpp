d4ae68d8e9ad695aed0e73e30b8a3b44a68e9bc4
Nina Recchia <itsnina@pm.me>
2026-01-29 03:34:29 -0500

    Add attribute info broadcasting to AttributeMenuWidgetController
    Introduces broadcasting of initial attribute values using a new AttributeInfoDelegate in AttributeMenuWidgetController. Adds support for finding and broadcasting primary strength attribute info, and exposes relevant properties and delegate for Blueprint use. Updates related UI Blueprints to support these changes.

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,12 +3,20 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AuraAttributeInfo.h"
+
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	check(AttributeInfo)
+	UAuraAttributeSet* AS = Cast<UAuraAttributeSet>(AttributeSet);
 	
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	
 }

a6915f6e27aa270ba5fc9e5a64067e224f8d031f
Nina Recchia <github@heynina.me>
2026-02-02 03:49:26 -0500

    Add character class information data asset and default info retrieval

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Votive Studios
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass) const
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}

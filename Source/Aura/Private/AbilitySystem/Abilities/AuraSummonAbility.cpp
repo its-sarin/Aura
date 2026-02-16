379869324ca2fde6c4557fa13282c8c13741085b
Nina Recchia <github@heynina.me>
2026-02-15 23:01:11 -0500

    Refactor GetSpawnLocations to calculate spawn points using line trace for ground detection

--- Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -3,7 +3,6 @@
 
 #include "AbilitySystem/Abilities/AuraSummonAbility.h"
 
-#include "Kismet/KismetSystemLibrary.h"
 
 TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
@@ -12,22 +11,24 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 	const float DeltaSpread = SpawnAngle / NumMinions;
 	
 	const FVector LeftOfSpawnAngle = Forward.RotateAngleAxis(-SpawnAngle * 0.5f, FVector::UpVector);
-	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + LeftOfSpawnAngle * MaxSpawnDistance, 5.f, FLinearColor::Green, 3.f);
-	DrawDebugSphere(GetWorld(), Location + LeftOfSpawnAngle * MinSpawnDistance, 15.f, 12, FColor::Red, false, 3.f);
-	DrawDebugSphere(GetWorld(), Location + LeftOfSpawnAngle * MaxSpawnDistance, 15.f, 12, FColor::Red, false, 3.f);
+	
+	TArray<FVector> SpawnLocations;
 	
 	for (int32 i = 0; i < NumMinions; ++i)
 	{
-		// TODO 
+		const FVector Direction = LeftOfSpawnAngle.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		
+		// Perform a line trace to find the ground below the chosen spawn location 
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0.f, 0.f, 500.f), ChosenSpawnLocation - FVector(0.f, 0.f, 500.f), ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			// If the line trace hits the ground, use the impact point as the spawn location
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
+		SpawnLocations.Add(ChosenSpawnLocation);
 	}
 	
-	const FVector RightOfSpawnAngle = Forward.RotateAngleAxis(SpawnAngle * 0.5f, FVector::UpVector);
-	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + RightOfSpawnAngle * MaxSpawnDistance, 5.f, FLinearColor::Blue, 3.f);
-	DrawDebugSphere(GetWorld(), Location + RightOfSpawnAngle * MinSpawnDistance, 15.f, 12, FColor::Red, false, 3.f);
-	DrawDebugSphere(GetWorld(), Location + RightOfSpawnAngle * MaxSpawnDistance, 15.f, 12, FColor::Red, false, 3.f);
-	
-	
-	
-	
-	return TArray<FVector>();
+	return SpawnLocations;
 }
